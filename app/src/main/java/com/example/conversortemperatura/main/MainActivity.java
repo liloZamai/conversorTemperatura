d82876d6d42f8687216813d00de2d873452bd8c5
Zamai <s2it_vcorrea@uolinc.com>
2020-03-02 10:40:34 -0300

    VERSÃO 1

---
+++ app/src/main/java/com/example/conversortemperatura/main/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.conversortemperatura.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.conversortemperatura.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText et_valor;
+    private Button btn_conv_faren_cels;
+    private TextView tv_convertido;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_valor = findViewById(R.id.et_valor);
+        btn_conv_faren_cels = findViewById(R.id.btn_farenheit_para_celsius);
+        tv_convertido = findViewById(R.id.tv_convertido);
+
+        btn_conv_faren_cels.setOnClickListener(this);
+    }
+
+    private void convFarenToCels(Double temp) {
+        try {
+            Double convertido = (temp-32)/1.8;
+            tv_convertido.setText(String.valueOf(convertido));
+        }catch (NumberFormatException e) {
+            Toast.makeText(this, "Não foi possível converter", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Aconteceu algo inesperado", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btn_conv_faren_cels) {
+            convFarenToCels(Double.valueOf(et_valor.getText().toString()));
+        }
+    }
+}
