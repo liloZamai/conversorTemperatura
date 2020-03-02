e0484f33dfe4f3b76d1ede12eba9949d6f74d841
Zamai <s2it_vcorrea@uolinc.com>
2020-03-02 10:55:30 -0300

    VERSÃO 2

--- app/src/main/java/com/example/conversortemperatura/main/MainActivity.java
+++ app/src/main/java/com/example/conversortemperatura/main/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private EditText et_valor;
     private Button btn_conv_faren_cels;
+    private Button btn_conv_cels_faren;
     private TextView tv_convertido;
 
     @Override
@@ -24,15 +25,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         et_valor = findViewById(R.id.et_valor);
         btn_conv_faren_cels = findViewById(R.id.btn_farenheit_para_celsius);
+        btn_conv_cels_faren = findViewById(R.id.btn_celsius_para_farenheit);
         tv_convertido = findViewById(R.id.tv_convertido);
 
         btn_conv_faren_cels.setOnClickListener(this);
+        btn_conv_cels_faren.setOnClickListener(this);
     }
 
     private void convFarenToCels(Double temp) {
         try {
             Double convertido = (temp-32)/1.8;
-            tv_convertido.setText(String.valueOf(convertido));
+            tv_convertido.setText(String.format("%.2f ºC", convertido));
+        }catch (NumberFormatException e) {
+            Toast.makeText(this, "Não foi possível converter", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Aconteceu algo inesperado", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void convCelsToFaren(Double temp) {
+        try {
+            Double convertido = 1.8 * temp + 32;
+            tv_convertido.setText(String.format("%.2f ºC", convertido));
         }catch (NumberFormatException e) {
             Toast.makeText(this, "Não foi possível converter", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
@@ -44,6 +58,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v == btn_conv_faren_cels) {
             convFarenToCels(Double.valueOf(et_valor.getText().toString()));
+        } else if (v == btn_conv_cels_faren) {
+            convCelsToFaren(Double.valueOf(et_valor.getText().toString()));
         }
     }
 }
